8e7d257109255819206fbc0f75cce1c4376332cc
Ameer Mansour <ameer.mansour@beca.com>
2022-12-14 11:37:34 +1300

    add desktop ui

---
+++ RetailManager/RetailManagerWpfUserInterface/Bootstrapper.cs
@@ -0,0 +1,47 @@
+﻿using Caliburn.Micro;
+using RetailManagerWpfUserInterface.ViewModels;
+using RetailManagerWpfUserInterface.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RetailManagerWpfUserInterface
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+
+        #region Fields
+
+
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Constructors
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        #endregion
+
+        #region Methods
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+
+        #endregion
+
+    }
+}
---
+++ RetailManager/RetailManagerWpfUserInterface/ViewModels/ShellViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailManagerWpfUserInterface.ViewModels
+{
+    internal class ShellViewModel
+    {
+
+        #region Fields
+
+
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Constructors
+
+
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+
+    }
+}
