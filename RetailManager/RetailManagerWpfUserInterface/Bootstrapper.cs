8e6afd6287b074aada531ba2d96adcbd566228c3
Ameer Mansour <ameer.mansour@beca.com>
2022-12-14 13:31:51 +1300

    add di config

--- RetailManager/RetailManagerWpfUserInterface/Bootstrapper.cs
+++ RetailManager/RetailManagerWpfUserInterface/Bootstrapper.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Input;
 
 namespace RetailManagerWpfUserInterface
 {
@@ -15,7 +16,7 @@ namespace RetailManagerWpfUserInterface
 
         #region Fields
 
-
+        SimpleContainer _container;
 
         #endregion
 
@@ -29,6 +30,7 @@ namespace RetailManagerWpfUserInterface
 
         public Bootstrapper()
         {
+            _container = new SimpleContainer();
             Initialize();
         }
 
@@ -36,9 +38,35 @@ namespace RetailManagerWpfUserInterface
 
         #region Methods
 
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes().Where(type => type.IsClass && type.Name.EndsWith("ViewModel")).ToList()
+                .ForEach(viewmodelType => _container.RegisterPerRequest(viewmodelType, viewmodelType.ToString(), viewmodelType));
+        }
+
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewForAsync<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
 
         #endregion
